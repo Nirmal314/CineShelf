0a54a71fa3561ef7d5c75c660e7f7373397b582b
Nirmal Ambasana <nambasana033@gmail.com>
2025-09-09 19:01:00 +0530

    Allowed image hostnames

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,34 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
       },
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+      },
+      {
+        protocol: "https",
+        hostname: "mosaic.scdn.co",
+      },
+      {
+        protocol: "https",
+        hostname: "wrapped-images.spotifycdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "developers.elementor.com",
+      },
+      {
+        protocol: "https",
+        hostname: "image-cdn-ak.spotifycdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "seed-mix-image.spotifycdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "image-cdn-fa.spotifycdn.com",
+      },
     ],
   },
 };
