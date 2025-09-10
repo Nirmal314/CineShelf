25d6f60316ef8670d318a1a28b3ab7ce349f82cd
Nirmal Ambasana <nambasana033@gmail.com>
2025-09-10 13:35:42 +0530

    Init DB, add drizzle

--- auth.ts
+++ auth.ts
@@ -1,7 +1,31 @@
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import Spotify from "next-auth/providers/spotify";
+import { db } from "./db";
+import { accounts, users } from "./db/schema";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+  }),
+  session: {
+    strategy: "jwt",
+  },
   providers: [Google, Spotify],
+  callbacks: {
+    async signIn({ user, account, profile, email }) {
+      return true; // Allow sign-in
+    },
+    async jwt({ token, user, account, profile, session }) {
+      return token;
+    },
+    async session({ session, token, user, newSession }) {
+      if (token.sub) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
 });
