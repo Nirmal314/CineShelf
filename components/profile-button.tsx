4f91c50f73a3c73503f0550663d6286ca81c4b7e
Nirmal Ambasana <nambasana033@gmail.com>
2025-09-26 14:37:24 +0530

    Undo a thing

--- components/profile-button.tsx
+++ components/profile-button.tsx
@@ -5,7 +5,6 @@ import { signOut } from "next-auth/react"
 import { Button } from "./ui/cool-button"
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 import Image from "next/image"
-import { useId } from "react"
 
 type Props = {
     image: string
@@ -14,26 +13,22 @@ type Props = {
 }
 
 export function ProfileButton({ image, name, email }: Props) {
-    const id = useId()
     return (
         <div className="fixed top-6 right-6 z-50">
             <Popover>
                 <PopoverTrigger asChild>
-                    <button aria-controls={id}>
-                        <Image
-                            src={image}
-                            alt={name}
-                            height={36}
-                            width={36}
-                            className="rounded-full shadow-lg cursor-pointer hover:shadow-xl transition-shadow"
-                        />
-                    </button>
+                    <Image
+                        src={image}
+                        alt={name}
+                        height={36}
+                        width={36}
+                        className="rounded-full shadow-lg cursor-pointer hover:shadow-xl transition-shadow"
+                    />
                 </PopoverTrigger>
 
                 <PopoverContent
                     align="end"
                     sideOffset={8}
-                    id={id}
                     className="w-64 bg-white/80 backdrop-blur-md rounded-2xl shadow-xl border border-gray-200
                     space-y-3
                     data-[state=open]:slide-in-from-left-20 
